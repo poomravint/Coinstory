6bc77e58ce702902cee4e36ac7ff5ae2dbcd46d7
poomravint <101710514+poomravint@users.noreply.github.com>
2026-01-29 20:50:49 +0700

    Connect today-expense API and Decorate

--- client/src/pages/Showtoday_expense.jsx
+++ client/src/pages/Showtoday_expense.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useState, useEffect, use } from "react";
+import Axios from "axios";
+
+import "./Showtoday_expense.css";
 
 const Showtoday_expense = () => {
-  return (
-    <div>Showtoday_expense</div>
-  )
-}
+  const [todayExpense, setTodayExpense] = useState([]);
 
-export default Showtoday_expense
+  // * Time Format
+  const formatDateTime = (dateString) =>
+    new Date(dateString).toLocaleString("th-TH", {
+      dateStyle: "medium",
+      timeStyle: "short",
+    });
+
+  // ! Get Today Expense API
+  const getTodayExpense = () => {
+    Axios.get(
+      `${import.meta.env.VITE_API_URL}/api/showTransaction/today-expense`,
+    ).then((response) => {
+      setTodayExpense(response.data.data || 0);
+    });
+  };
+
+  useEffect(() => {
+    getTodayExpense();
+  });
+
+  return (
+    <>
+      <div className="expense-table">
+        {todayExpense.map((item) => (
+          <div className="today-expense-box">
+            <div className="top-content">
+              <p>Type : {item.category}</p>
+              <p>
+                <strong>{item.amount}</strong> THB
+              </p>
+            </div>
+            {item.note && (
+              <div className="middle-content">
+                <p>Note : {item.note}</p>
+              </div>
+            )}
+            <div className="buttom-content">
+              <p>{formatDateTime(item.action_at)}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Showtoday_expense;
