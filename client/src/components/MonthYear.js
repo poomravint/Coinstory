77160f0042ffd16ac6cd838686621e97f71e150e
Pennapa Butda <poomravintwongwaro@gmail.com>
2026-02-02 08:16:13 +0700

    Add MonthYear.js for select

---
+++ client/src/components/MonthYear.js
@@ -0,0 +1,16 @@
+export const months = [
+  { value: "01", label: "Jan" },
+  { value: "02", label: "Feb" },
+  { value: "03", label: "Mar" },
+  { value: "04", label: "Apr" },
+  { value: "05", label: "May" },
+  { value: "06", label: "Jun" },
+  { value: "07", label: "Jul" },
+  { value: "08", label: "Aug" },
+  { value: "09", label: "Sep" },
+  { value: "10", label: "Oct" },
+  { value: "11", label: "Nov" },
+  { value: "12", label: "Dec" },
+];
+
+export const years = ["2024", "2025", "2026","2027"];
--- client/src/pages/IncomeExpenseList.jsx
+++ client/src/pages/IncomeExpenseList.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import "./IncomeExpenseList.css";
 
+import {months} from "../components/MonthYear";
+import {years} from "../components/MonthYear";
+
 const TxRxList = () => {
   const [incomebtn, setIncomeBtn] = useState(true);
   const now = new Date();
@@ -34,18 +37,11 @@ const TxRxList = () => {
           value={month}
           onChange={(e) => setMonth(e.target.value)}
         >
-          <option value="01">Jan</option>
-          <option value="02">Feb</option>
-          <option value="03">Mar</option>
-          <option value="04">Apr</option>
-          <option value="05">May</option>
-          <option value="06">Jun</option>
-          <option value="07">Jul</option>
-          <option value="08">Aug</option>
-          <option value="09">Sep</option>
-          <option value="10">Oct</option>
-          <option value="11">Nov</option>
-          <option value="12">Dec</option>
+          {months.map((m) => (
+            <option key={m.value} value={m.value}>
+              {m.label}
+            </option>
+          ))}
         </select>
 
         <select
@@ -53,9 +49,11 @@ const TxRxList = () => {
           value={year}
           onChange={(e) => setYear(e.target.value)}
         >
-          <option value="2024">2024</option>
-          <option value="2025">2025</option>
-          <option value="2026">2026</option>
+          {years.map((y) => (
+            <option key= {y} value={y}>
+              {y}
+            </option>
+          ))}
         </select>
       </div>
     </>
